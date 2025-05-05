cae05c7ba0802ef331bb823601aa163e86a54153
julian <julianandresac@gmail.com>
2025-05-05 10:23:53 -0500

    primer commit

---
+++ src/main/java/co/edu/ufps/segundoparcial2025/SegundoParcial2025Application.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.segundoparcial2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoParcial2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoParcial2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/ufps/segundoparcial2025/SegundoParcial2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.segundoparcial2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoParcial2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
