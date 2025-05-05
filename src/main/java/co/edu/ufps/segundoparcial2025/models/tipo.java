39dce6810ad4ed4a18327b85a288588cce14bfa2
julian <julianandresac@gmail.com>
2025-05-05 11:04:51 -0500

    Se crearon todas las entidades

--- src/main/java/co/edu/ufps/segundoparcial2025/models/tipo.java
+++ src/main/java/co/edu/ufps/segundoparcial2025/models/tipo.java
@@ -1,4 +1,20 @@
 package co.edu.ufps.segundoparcial2025.models;
 
-public class tipo {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+public class Tipo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+
+    @OneToMany(mappedBy = "tipo")
+    private List<Manga> mangas;
+
 }
